6111664f6ddf5b8a30ea55b3c42531f6001ebbe3
yalavrinenko <lavrinenko@phystech.edu>
2021-12-27 18:57:27 +0300

    add number for upper bound

--- 24/test.py
+++ 24/test.py
@@ -14,6 +14,8 @@ def z_value(z, w, d):
     print("Digit = {4}: Current x_add={2} w_add={3}.\tNext x_add={0} w_add={1}".format(x_add[d+1], w_add[d + 1], x_add[d], w_add[d], d))
     print(z)
 
+# num = [9, 2, 9, 6, 9, 5, 9, 3, 4, 9, 7, 9, 9, 2]
+
 num = [8, 1, 5, 1, 4, 1, 7, 1, 1, 6, 1, 3, 8, 1]
 for i, c in enumerate(num):
     z_value(z, c, i)
